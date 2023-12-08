07e4bcaf720de25e7f55ca6080afad369ad11360
michelleammoise <mmoise6@gatech.edu>
2023-12-08 09:16:28 -0500

    Update files with animations and images

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var burger = document.querySelector('.burger');
+var banner = document.querySelector('.banner');
 var navbar = document.querySelector('.navbar');
 var navleftside = document.querySelector('.nav-left-side');
 var navrightside = document.querySelector('.nav-right-side');
@@ -6,10 +6,14 @@ var navrightside = document.querySelector('.nav-right-side');
 
 
 
-burger.addEventListener('click', () => {
+banner.addEventListener('click', () => {
   navrightside.classList.toggle('v-class');
   navleftside.classList.toggle('v-class');
   navbar.classList.toggle('h-nav');
   // anchor.classList.toggle('v-class')
 })
+
+
+/* create a simple page animation that fade transitions to next page when click */
+// Path: script.js
 
